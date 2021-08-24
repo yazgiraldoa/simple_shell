b5f313f1f76d49f0ad4fd98c9c5699a5470b9005
IbetheRamada <valerdagd@gmail.com>
2021-08-24 16:54:09 -0500

    Add the files builtins count_words and prompt

---
+++ builtins.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+* get_builtin - Function that look for a builtin (env, exit, etc).
+* @string: string to be evaluated as builtin.
+* Return: A function associated with the builtin name (success).
+*/
+
+int (*get_builtin(char *string))()
+{
+	unsigned int j = 0;
+
+	while (list[j].name != NULL)
+	{
+		if ((_strcmp(string, list[j].name)) == 0)
+		{
+			return (list[j].func);
+		}
+		j++;
+	}
+	return (NULL);
+}
+
+/**
+ * env - function that prints environment variables.
+ * Return: Success is 0
+ */
+
+int env(void)
+{
+	int i = 0;
+
+	for (i = 0; environ[i]; i++)
+	{
+		write(1, environ[i], _strlen(environ[i]));
+		write(1, "\n", 1);
+	}
+	return (0);
+}
+
+/**
+ * _own_exit - function that exit the terminal.
+ * Return: 1 as a flag to return in main function.
+ */
+
+int _own_exit(void)
+{
+	return (1);
+}
---
+++ count_words.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#define OUT 0
+#define IN 1
+
+/**
+ * count_words - function that counts words depending on delimiters
+ * @str: string to be evaluated.
+ * Return: number of words in the string.
+ */
+
+int count_words(char *str)
+{
+	int state = OUT;
+	unsigned int wc = 0;
+
+	while (*str)
+	{
+		if (*str == ' ' || *str == '\n' || *str == '\t'
+			|| *str == ':' || *str == '=')
+			state = OUT;
+
+		else if (state == OUT)
+		{
+			state = IN;
+			++wc;
+		}
+		++str;
+	}
+	return (wc);
+}
